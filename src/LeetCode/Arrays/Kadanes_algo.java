0959b375ecdbcbbb2877b7e13a2925fb0062bc3d
Tarunkaul <tarunkaul03@gmail.com>
2026-04-19 11:22:39 +0530

    Kadane's Algorithm

---
+++ src/LeetCode/Arrays/Kadanes_algo.java
@@ -0,0 +1,13 @@
+package LeetCode.Arrays;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0,max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+            max = Math.max(sum,max);
+            if(sum < 0) sum = 0; // if sum goes to negative then sum = 0
+        }
+        return max;
+    }
+}
