e2d901012673f1eecaa52bbb960f24b86050f1a6
Tarunkaul <tarunkaul03@gmail.com>
2026-05-24 23:43:35 +0530

    ADominoPiling

---
+++ src/CodeForces/CF Questions/ADominoPiling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ADominoPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int temp = x * y;
+        System.out.println(temp / 2);
+    }
+}
